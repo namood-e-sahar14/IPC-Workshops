141dee3b977d7d8cf0f5e84efbaece1e6eee620e
binte <namood-e-sahar@myseneca.ca>
2021-01-29 09:36:08 -0500

    part 1 initial changes

--- WS02/p1/w2p1.c
+++ WS02/p1/w2p1.c
@@ -1,11 +1,41 @@
 /*
-	==================================================
-	Workshop #2 (Part-1):
-	==================================================
-	Name   :
-	ID     :
-	Email  :
-	Section:
+    ==================================================
+    Workshop #2 (Part-1):
+    ==================================================
+    Name   :Namood-e-sehar
+    ID     :121979207
+    Email  :namood-e-sahar@myseneca.ca
+    Section:NFF
 */
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int cents, coins;
+    double dollars, Amount, value, remaining;
 
+    scanf("%d", &cents);
+    scanf("%.2lf" & dollars);
+    printf("change Maker Machine");
+    printf("=====================");
+    scanf("%.2lf" & Amount);
+    printf("Enter dollars and cents amount to convert to coins: 9.92");
+    scanf("%.2lf" & remaining);
+
+    value = > 2;
+    coins = value / 2;
+    value = < 2 && value = >1;
+    coins = value / 1;
+    value = > 0.25;
+    coins = value / 0.25;
+
+    printf("coins=%d", coins);
+    scanf("%d", &coins);
+
+    remaining = value % Amount;
+    printf("remaining=%d\n", remaining);
+    scanf("%.2lf" & remaining);
+
+    return 0;
+}
