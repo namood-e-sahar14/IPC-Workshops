cc47ff1f37ccff13e6b2cefbdbf6ed0a31830d4f
binte <namood-e-sahar@myseneca.ca>
2021-01-30 10:35:57 -0500

    final changes

--- WS02/p1/w2p1.c
+++ WS02/p1/w2p1.c
@@ -12,11 +12,11 @@
 #include<math.h>
 int main()
 {
-    int cents, coins, tc, lc, qc;
-    double toonies, // this will store the number of toonies in the amount
-         
-        amount, // this
-        loonies, remaining,quarter;
+    int toonies, // this will store the number of toonies in the amount
+        loonies,
+        quarter;
+    double amount, // this
+        remaining;
 
     // Prompt user for the input
     printf("Change Maker Machine \n");
@@ -25,28 +25,24 @@ int main()
     scanf("%lf", &amount);
 
     // Calculate Toonies by dividing to 2 and display to user
-    toonies = (amount / 2);
-   tc = toonies - 0.5;
- 
-    printf("\n\n$2.00 Toonies X =%d", tc);
-    // Calculate remaining by subtracting the total amount of toonies from main amount
-    remaining = amount - (tc * 2);
-    printf("(remaining: $=%.2lf) \n", remaining);
-
-   
-    loonies = (remaining  / 1);
-    lc = loonies - 0.5;
-    printf("$ 1.00 Loonie x =%d ", lc);
-    remaining =remaining - (lc * 1);
-    printf("(remaining: $=%.2lf) \n", remaining);
-   
-    quarter = (remaining / 0.25);
-    qc = quarter - 0.5;
-    printf("$ 0.25 quarter x =%d ",qc);
-    remaining = remaining- (qc *0.25);
-    printf("(remaining: $=%.2lf) \n", remaining);
+    toonies = (int)(amount / 2);
     
-    printf("Machine Error! Thankyou for letting me Keep change = $ %.2lf \n", remaining);
+    printf("\n\n$2.00 Toonies X %d ", toonies);
+    // Calculate remaining by subtracting the total amount of toonies from main amount
+    remaining = amount - (toonies * 2);
+    printf("(remaining: $%.2lf) \n", remaining);
+   
+    loonies = (int)(remaining  / 1);
+    printf("$1.00 Loonies X %d ", loonies);
+    remaining =remaining - (loonies * 1);
+    printf("(remaining: $%.2lf) \n", remaining);
+   
+    quarter = (int)(remaining / 0.25);
+    printf("$0.25 Quarters X %d ", quarter);
+    remaining = remaining - (quarter * 0.25);
+    printf("(remaining: $%.2lf) \n", remaining);
+    
+    printf("<nMachine Error! Thankyou for letting me keep $%.2lf! \n", remaining);
 
  
     return 0;
--- WS02/p2/w2p2.c
+++ WS02/p2/w2p2.c
@@ -1,11 +1,53 @@
 /*
-	==================================================
-	Workshop #2 (Part-2):
-	==================================================
-	Name   :
-	ID     :
-	Email  :
-	Section:
+    ==================================================
+    Workshop #2 (Part-2):
+    ==================================================
+    Name   :Namood-e-sehar
+    ID     :121979207
+    Email  :namood-e-sahar@myseneca.ca
+    Section:NFF
 */
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int cents, coins, tc, lc, qc;
+    double toonies, // this will store the number of toonies in the amount
 
+        amount, // this
+        loonies, remaining, quarter;
+
+    // Prompt user for the input
+    printf("Change Maker Machine \n");
+    printf("==================== \n");
+    printf("Enter dollars and cents amount to convert to coins: $");
+    scanf("%lf", &amount);
+
+    // Calculate Toonies by dividing to 2 and display to user
+    toonies = (amount / 2);
+    tc = toonies - 0.5;
+
+    printf("\n\n$2.00 Toonies X =%d", tc);
+    // Calculate remaining by subtracting the total amount of toonies from main amount
+    remaining = amount - (tc * 2);
+    printf("(remaining: $=%.2lf) \n", remaining);
+
+
+    loonies = (remaining / 1);
+    lc = loonies - 0.5;
+    printf("$ 1.00 Loonie x =%d ", lc);
+    remaining = remaining - (lc * 1);
+    printf("(remaining: $=%.2lf) \n", remaining);
+
+    quarter = (remaining / 0.25);
+    qc = quarter - 0.5;
+    printf("$ 0.25 quarter x =%d ", qc);
+    remaining = remaining - (qc * 0.25);
+    printf("(remaining: $=%.2lf) \n", remaining);
+
+    printf("Machine Error! Thankyou for letting me Keep change = $ %.2lf \n", remaining);
+
+
+    return 0;
+}
