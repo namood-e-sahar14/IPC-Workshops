9ab22cd0b0137fb043f83ae524ec251090569e5b
binte <namood-e-sahar@myseneca.ca>
2021-02-05 15:20:39 -0500

    atlast final

--- WS02/p2/w2p2.c
+++ WS02/p2/w2p2.c
@@ -24,60 +24,60 @@ int main()
         service;
 
     // Prompt user for the input
-    printf("Change Maker Machine \n");
-    printf("==================== \n");
+    printf("Change Maker Machine\n");
+    printf("====================\n");
     printf("Enter dollars and cents amount to convert to coins: $");
     scanf("%lf", &amount);
 
     // calculate 5% of the amount
     service = 0.50;
-    printf("Service Fee (5.0 percent): $%.2lf \n", service);
+    printf("Service fee (5.0 percent): %.2lf\n", service);
 
     // subtract the 5% chargee from  the amount
     amount = amount - service;
-    printf("Balance to dispense: $%.2lf \n", amount);
+    printf("Balance to dispense: $%.2lf\n", amount);
    
     // Calculate Toonies by dividing to 2 and display to user
     toonies = (int)(amount / 2);
-    printf("\n\n$2.00 Toonies X %d ", toonies);
+    printf("\n$2.00 Toonies  X %d ",toonies);
     // Calculate remaining by subtracting the total amount of toonies from main amount
     remaining = amount - (toonies * 2);
-    printf("(remaining: $%.2lf) \n", remaining);
+    printf("(remaining: $%.2lf)\n", remaining);
 
     // Calculate loonies by dividing to 1 and display to user
     loonies = (int)(remaining / 1);
-    printf("$1.00 Loonies X %d ", loonies);
+    printf("$1.00 Loonies  X %d ",loonies);
     // Calculate remaining by subtracting the total amount of loonies from remaining 
     remaining = remaining - (loonies * 1);
-    printf("(remaining: $%.2lf) \n", remaining);
+    printf("(remaining: $%.2lf)\n", remaining);
 
     // Calculate quarters by dividing to 0.25 and display to user
     quarter = (int)(remaining / 0.25);
     printf("$0.25 Quarters X %d ", quarter);
     // Calculate remaining by subtracting the total amount of quarters from remaining
     remaining = remaining - (quarter * 0.25);
-    printf("(remaining: $%.2lf) \n", remaining);
+    printf("(remaining: $%.2lf)\n", remaining);
 
     // Calculate dimes by dividing to 0.10 and display to user
     dime = (int)(remaining / 0.10);
-    printf("$0.10 Dimes X %d ", dime);
+    printf("$0.10 Dimes    X %d ", dime);
     // Calculate remaining by subtracting the total amount of dimes from remaining
     remaining = remaining - (dime * 0.10);
-    printf("(remaining: $%.2lf) \n", remaining);
+    printf("(remaining: $%.2lf)\n", remaining);
 
     // Calculate nickles by dividing to 0.05 and display to user
     nickle = (int)(remaining / 0.05);
-    printf("$0.05 Nikles X %d ", nickle);
+    printf("$0.05 Nickels  X %d ", nickle);
     // Calculate remaining by subtracting the total amount of nickles from remaining
     remaining = remaining - (nickle * 0.05);
-    printf("(remaining: $%.2lf) \n", remaining);
+    printf("(remaining: $%.2lf)\n", remaining);
     
     // Calculate pennies by dividing to 0.01 and display to user
     penny = (remaining / 0.01);
-    printf("$0.01 Pennies X %.0lf ", penny);
+    printf("$0.01 Pennies  X %.0lf ", penny);
     // Calculate remaining by subtracting the total amount of pennies from remaining
     remaining = remaining - (penny * 0.01);
-    printf("(remaining: $%.2lf) \n", remaining);
+    printf("(remaining: $%.2lf)\n", remaining);
 
     printf("\nAll coins dispensed!\n");
 
--- WS03/p1/w3p1.c
+++ WS03/p1/w3p1.c
@@ -2,10 +2,91 @@
 	==================================================
 	Workshop #3 (Part-1):
 	==================================================
-	Name   :
-	ID     :
-	Email  :
-	Section:
+	Name   : Namood-e-sehar
+	ID     :121979207
+	Email  :Namood-e-sahar@myseneca.ca
+	Section:NFF
 */
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
 
+#define MIN_YEAR 2010	
+#define MAX_YEAR 2021
+
+int main()
+{
+	const int JAN = 1;
+	const int DEC = 12;
+
+	int log_month, log_year;
+	
+	printf("General Well_being Log\n");
+	printf("======================\n");
+
+	char execute = 'y';
+	while (execute == 'y')
+	{
+		printf("Set the year and month for the well-being log (YYYY MM): ");
+		int inputs = scanf("%d %d", &log_year, &log_month);
+
+		if (log_year < MIN_YEAR || log_year > MAX_YEAR)
+		{
+			printf("ERROR: the year must be between 2010 and 2021 inclusive\n");
+		} else if (log_month < JAN || log_month > DEC)
+		{
+			printf("ERROR: Jan.(1) - Dec.(12)\n");
+		}
+		else {
+			execute = 'n';
+		}
+	}
+
+	char log_month_name[4] = "DEC";
+	switch (log_month)
+	{
+		case 1:
+			strcpy(log_month_name, "JAN");
+			break;
+		case 2:
+			strcpy(log_month_name, "FEB");
+			break;
+		case 3:
+			strcpy(log_month_name, "MAR");
+			break;
+		case 4:
+			strcpy(log_month_name, "APR");
+			break;
+		case 5:
+			strcpy(log_month_name, "MAY");
+			break;
+		case 6:
+			strcpy(log_month_name, "JUN");
+			break;
+		case 7:
+			strcpy(log_month_name, "JUL");
+			break;
+		case 8:
+			strcpy(log_month_name, "AUG");
+			break;
+		case 9:
+			strcpy(log_month_name, "SEP");
+			break;
+		case 10:
+			strcpy(log_month_name, "OCT");
+		case 11:
+			strcpy(log_month_name, "NOV");
+			break;
+		case 12:
+			strcpy(log_month_name, "DEC");
+			break;
+		default:
+			break;
+	}
+
+	printf("\n\n*** Log date set! ***\n\n");
+	printf("Log starting date: %d-%s-0%d\n\n", log_year, log_month_name, 1);
+		
+	return 0;
+
+}
